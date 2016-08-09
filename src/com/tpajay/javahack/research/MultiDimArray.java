f0a46ae403030afa3b1effe62daafc1c92c25818
Jason Muse <jason.muse@gmail.com>
2016-08-09 13:00:14 -0400

    package updates

---
+++ src/com/tpajay/javahack/research/MultiDimArray.java
@@ -0,0 +1,42 @@
+package com.tpajay.javahack.research;
+
+public class MultiDimArray 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        String[] arrayTest = new String[] {"Bob","Kim","Johnny","Susie"};
+        
+        for(int i=0; i < arrayTest.length; i++) {
+        	System.out.println("arrayTest: " + i + ":" + arrayTest[i]);
+        }
+        
+        System.out.println("---------------------------------");
+        		
+        //multidimensional array
+        String[][] arrayTest2 = new String[][] { {"Names","John",null}, { "Names2", "Bob","Kim","Johnny","Susie" } };
+        
+        //0,1: John
+        //0,2: null
+        //1,1: Bob
+        //1,2: Kim
+        //1,3: Johnny
+        //1,4: Susie
+        //System.out.println("arrayTest2: " + arrayTest2[1][4]);
+        for(int i=0; i < arrayTest2.length; i++) {
+        	for (int j=1; j < arrayTest2[i].length; j++) {
+        		System.out.println("arrayTest2, value at [" + i + "][" + j + "] is: " + arrayTest2[i][j]);
+        	}
+        }
+
+        System.out.println("---------------------------------");
+   
+        String multiStrArr[][] = new String[][]{
+        	{"A", "B"},
+        	null,
+        	{"Jan", "Feb", "Mar"},
+        	};
+        
+    }
+}
